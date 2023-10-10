b2fc6cfce7f5f0f90b9802f6e51e1639bccf2b3a
vkudravtsev <u.kudrautsau@softteco.com>
2023-10-10 11:59:32 +0300

    chore: end execution if file doesn't exist

--- src/storage/fs-storage.ts
+++ src/storage/fs-storage.ts
@@ -5,9 +5,6 @@ export class FileStorage implements Storage {
   constructor(private readonly source: string) {}
 
   public async save(data: string): Promise<void> {
-    return appendFile(this.source, data)
-    .catch(err => {
-      console.log(err);
-    })
+    return appendFile(this.source, data);
   }
 }
